80ba12e7e6bc88baa4d280572fadfbf6bc67e69f
rfboulier <pro.boulier.romane@gmail.com>
2026-03-06 15:35:08 +0100

    (feature) : modification des routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {Accueil} from './shared/accueil/accueil';
+import {Connexion} from './features/connexion/connexion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: Accueil},
+  {path: 'connexion', component: Connexion},
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Connexion} from './features/connexion/connexion';
+import {Navbar} from './shared/navbar/navbar';
+import {Accueil} from './shared/accueil/accueil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Connexion, Navbar, Accueil],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
