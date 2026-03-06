a067352864d2b4c121f7fd55fcedd7fb4fe1e41d
Romane F. BOULIER <pro.boulier.romane@gmail.com>
2026-03-06 22:38:25 +0100

    (ui) : amélioration CSS

--- src/app/services/characterService.ts
+++ src/app/services/characterService.ts
@@ -14,8 +14,7 @@ export class CharacterService {
   private readonly baseUrl: string = 'https://dragonball-api.com/api';
 
   public getAllCharacters(): Observable<Character[]> {
-    return this.http.get<any>(`${this.baseUrl}/characters`)
+    return this.http.get<any>(`${this.baseUrl}/characters?limit=50`)
       .pipe(map((response) => response.items));
   }
-
 }
